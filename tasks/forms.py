ed84cd4d8abd39419a1994f92834e72f884301f8
Zakhar Ermishkin <z.ermishkin@dunice.net>
2019-08-12 15:01:50 +0300

    format

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from .models import Task
 
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ('title', 'desc', 'id')
-        
+
     def __init__(self, *args, **kwargs):
         super(TaskForm, self).__init__()
-        self.fields['title'].widget.attrs.update({'class' : 'form-control'})
+        self.fields['title'].widget.attrs.update({'class': 'form-control'})
         self.fields['title'].label = 'Название таска'
-        self.fields['desc'].widget.attrs.update({'class' : 'form-control'})
+        self.fields['desc'].widget.attrs.update({'class': 'form-control'})
         self.fields['desc'].label = 'Описание таска'
